eb2f77f664aa8470d654c1ee53c2debb9e46894b
Abu Naim <abunaim@Abus-MacBook-Air-2.local>
2023-11-21 16:38:25 +0600

    add_method

--- shop.py
+++ shop.py
@@ -1,8 +1,27 @@
+from abc import ABC, abstractmethod
 class Shop:
-    def __init__(self) -> None:
-        pass
+    product = []
+    
+    def __init__(self, product_name, price) -> None:
+        self.product_name = product_name
+        self.price = price
 
-class Product:
-    def __init__(self) -> None:
+    @abstractmethod
+    def add_product(self):
         pass
+        
+        
 
+class Product(Shop):
+    def __init__(self, product_name, price, quantity) -> None:
+        self.quantity = quantity
+        super().__init__(product_name, price)
+
+    def add_product(self):
+        product = {'product_name': self.product_name, 'price': self.price, 'quantity': self.quantity}
+        self.product.append(product)
+
+    
+
+
+# Inside the Shop class, create a method name add_product which adds products using the Product class to the Shop class.
