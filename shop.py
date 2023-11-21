9aad200cc5fa77e12af4d84ac66d1125042bda54
Abu Naim <abunaim@Abus-MacBook-Air-2.local>
2023-11-22 00:00:25 +0600

    product add

--- shop.py
+++ shop.py
@@ -1,27 +1,32 @@
-from abc import ABC, abstractmethod
-class Shop:
-    product = []
-    
-    def __init__(self, product_name, price) -> None:
+class Product:
+    def __init__(self, product_name, price, quantity) -> None:
         self.product_name = product_name
         self.price = price
-
-    @abstractmethod
-    def add_product(self):
-        pass
-        
-        
-
-class Product(Shop):
-    def __init__(self, product_name, price, quantity) -> None:
         self.quantity = quantity
-        super().__init__(product_name, price)
+    def __repr__(self) -> str:
+        return f'Product Name: {self.product_name}, Price: {self.price}, Quantity: {self.quantity}'
 
-    def add_product(self):
-        product = {'product_name': self.product_name, 'price': self.price, 'quantity': self.quantity}
-        self.product.append(product)
+class Shop:
+    def __init__(self, name) -> None:
+        self.name = name
+        self.products = []
 
-    
+    def add_product(self, product_name, price, quantity):
+        product = Product(product_name, price, quantity)
+        self.products.append(product)
+        
+    def __repr__(self) -> str:
+        print("Our Products")
+        for product in self.products:
+            print(product)
+        return f'All done'   
 
+kamal = Shop('Kamal Banana Shop')
+kamal.add_product('Banana Khete moja', 10, '2 hali')
+kamal.add_product('Tetul Tok', 22, '2kg')
+kamal.add_product('Anarosh', 33, '10 piece')
+kamal.add_product('Kumrar Bichi', 44, '40kg')
 
-# Inside the Shop class, create a method name add_product which adds products using the Product class to the Shop class.
+print(kamal)
+        
+
