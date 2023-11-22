4665d2d07b64c082f8f57192c43652c301b6fc5e
Abu Naim <abunaim@Abus-MacBook-Air-2.local>
2023-11-22 16:46:19 +0600

    add_buy

--- shop.py
+++ shop.py
@@ -7,26 +7,32 @@ class Product:
         return f'Product Name: {self.product_name}, Price: {self.price}, Quantity: {self.quantity}'
 
 class Shop:
-    def __init__(self, name) -> None:
-        self.name = name
+    def __init__(self) -> None:
         self.products = []
-
+    
     def add_product(self, product_name, price, quantity):
         product = Product(product_name, price, quantity)
         self.products.append(product)
-        
-    def __repr__(self) -> str:
-        print("Our Products")
+
+    def buy_product(self, name, quantity):
         for product in self.products:
-            print(product)
-        return f'All done'   
+            if product.product_name == name and product.quantity >= quantity:
+                print(f'Here is your product: {name} and total price is {quantity*product.price}')
+    def __repr__(self) -> str:
+        return f'Dhonnobad abar ashben, abar ashben'
+            
+apple = Shop()
+apple.add_product('iphone Xs', 2500, 100)
+apple.add_product('iphone 12', 23542, 12)
+apple.add_product('iphone 13', 2534, 10)
+apple.add_product('iphone 15 pro', 542, 90)
+apple.add_product('iphone 22 pro', 3252, 120)
 
-kamal = Shop('Kamal Banana Shop')
-kamal.add_product('Banana Khete moja', 10, '2 hali')
-kamal.add_product('Tetul Tok', 22, '2kg')
-kamal.add_product('Anarosh', 33, '10 piece')
-kamal.add_product('Kumrar Bichi', 44, '40kg')
+apple.buy_product('iphone Xs', 5)
 
-print(kamal)
+print(apple)
+
+
+# print(kamal)
         
 
